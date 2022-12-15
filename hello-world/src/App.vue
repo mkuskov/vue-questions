5266019fdfc2d0cfaa525d005f2898d09db9f22b
Матвей Кусков <73180631+mkuskov@users.noreply.github.com>
2022-12-15 11:19:50 +0300

    готова демо версия сайта interview

---
+++ hello-world/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <header>
+    <p class="interview-title">Interview</p>
+    <router-link to="/" class="list">Список опрошенных</router-link>
+    <router-link to="/questions" class="pass">Пройти опрос</router-link>
+  </header>
+  <router-view class="content"></router-view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isStarted: JSON.parse(localStorage.getItem('isInterviewStarted')),
+      localStorageData: localStorage.length,
+    }
+  },
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  background-color: #ececec;
+}
+
+header {
+  margin-top: 1.5%;
+  text-align: center;
+  width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template-columns: 4.5fr 1fr 1fr;
+}
+
+.interview-title {
+  text-decoration: none;
+  font-size: 32px;
+  line-height: 28px;
+  color: #000000;
+  margin: 0;
+  width: 10%;
+  color: rgb(15, 15, 15);
+}
+
+#app {
+  font-family: 'Inter', sans-serif;
+  border-radius: 15px;
+}
+
+.list {
+  margin-right: 12px;
+}
+
+.pass,
+.list {
+  font-size: 16px;
+  text-decoration: none;
+  cursor: pointer;
+  transition: .2s;
+  padding: 10px;
+  border-radius: 5px;
+  color: white;
+  border: none;
+}
+
+.pass {
+  width: 150px;
+  background-color: #27AE60;
+}
+
+.list {
+  width: 180px;
+  background-color: #2F54EB;
+}
+
+.list:hover {
+  transition: .2s;
+  background-color: #203fb9;
+}
+
+.pass:hover {
+  transition: .2s;
+  background-color: #218f4f;
+}
+</style>
---
+++ hello-world/src/components/Questions.vue
@@ -0,0 +1,475 @@
+<template>
+    <main id="registration" v-if="localStorageData === 0" v-for="items in renderRegistration">
+        <Form @submit="onSubmit">
+            <div class="container">
+                <h4>Site&#160; / &#160;Interview</h4>
+                <div class="fields">
+                    <p>Full Name</p>
+                    <Field name="name" type="name" :rules="validateName" v-model="name" class="field"
+                        placeholder="Full Name" />
+                    <ErrorMessage name="name" class="errors" />
+                    <p>E-mail</p>
+                    <Field name="email" type="email" :rules="validateEmail" v-model="email" class="field"
+                        placeholder="example@email.com" />
+                    <ErrorMessage name="email" class="errors" />
+                    <p>Phone</p>
+                    <Field name="tel" type="tel" :rules="validateTel" v-model="tel" class="field"
+                        placeholder="+7__________" />
+                    <ErrorMessage name="tel" class="errors" />
+                    <div class="buttons">
+                        <router-link to="/" class="button-back">Вернуться к
+                            списку
+                        </router-link>
+                        <button class="button-login" @click="persist(); updateRegistration();" @keypress.enter="persist"
+                            :disabled='isDisabled'>Начать опрос
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </Form>
+    </main>
+    <div id="questions" v-for="items in renderQuestions">
+        <div class="content">
+            <h4>Site &#160; / &#160; Interview</h4>
+            <p>{{ title }}</p>
+            <div class="progressbar">
+                <div class="steps" v-for="questionsCount in questions">
+                    <hr class="progress-step" :class="{ progressStepCurrent: questionsCount.id === questionIndex + 1 }">
+                    <span class="progress-point" @click="questionIndex = questionsCount.id - 1">
+                        <img src="../images/state.png" alt="progress" class="points"
+                            v-if="questionsCount.id > questionIndex + 1">
+                        <img src="../images/current.png" alt="progress" class="points"
+                            v-if="questionsCount.id === questionIndex + 1">
+                        <img src="../images/done.png" alt="progress" class="points"
+                            v-if="questionsCount.id < questionIndex + 1">
+                        <h2>Вопрос {{ questionsCount.id }}&#160;</h2>
+                    </span>
+                </div>
+            </div>
+            <div class="container-quest">
+                <div v-for="(questions, index) in questions">
+                    <div v-show="index === questionIndex">
+                        <div class="quests">
+                            <p v-for="title in questions.text">{{ title }}</p>
+                            <br>
+                            <label v-for="answer in questions.responses" v-if="questionIndex === 0" class="choose">
+                                <input type="radio" v-model="radios" :id="answer.id" :value="answer.points">
+                                {{ answer.text }}
+                            </label>
+                            <label v-for="answer in questions.responses" v-if="questionIndex === 1" class="choose">
+                                <input type="number" v-model="months" :id="answer.id">
+                                {{ answer.text }}
+                            </label>
+                            <label v-for="answer in questions.responses" v-if="questionIndex === 2" class="choose">
+                                <input type="checkbox" v-model="rainbow" :id="answer.id" :value="answer.points">
+                                {{ answer.text }}
+                            </label>
+                            <select v-if="questionIndex === 3" class="select" v-model="particles">
+                                <option v-for="answer in questions.responses" type="checkbox" :id="answer.id"
+                                    :value="answer.points">
+                                    {{ answer.text }}
+                                </option>
+                            </select>
+                            <label v-for="answer in questions.responses" v-if="questionIndex === 4" class="choose">
+                                <input type="text" v-model="universe" :id="answer.id">
+                            </label>
+                            <button v-if="questionIndex > 0" v-on:click="prev" class="btn-prev">
+                                Вернуться назад
+                            </button>
+                            <button v-if="questionIndex < 4" v-on:click="next" class="btn-next">
+                                Следующий
+                            </button>
+                            <router-link to="/" v-if="questionIndex === 4" v-on:click="addCurrentResults();"
+                                class="btn-next">
+                                Завершить опрос
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { Form, Field, ErrorMessage } from 'vee-validate';
+export default {
+    data() {
+        return {
+            localStorageData: localStorage.length,
+            isInterviewStarted: false,
+            renderRegistration: ['1'],
+            renderQuestions: localStorage.length > 0 ? ['1'] : [],
+            title: 'Вопросы:',
+            questionIndex: 0,
+            radios: 0,
+            months: 0,
+            rainbow: [],
+            particles: 0,
+            universe: 0,
+            name: '',
+            email: '',
+            tel: '',
+            results: [],
+            questions: [
+                {
+                    text: ["Какого числа Хэллоуин?"],
+                    id: 1,
+                    responses: [
+                        { text: '31 Октября', points: 3 },
+                        { text: '1 Ноября', points: 2 },
+                        { text: '1 Октября', points: 1 },
+                    ]
+                }, {
+                    text: ["Сколько месяцев в году?"],
+                    id: 2,
+                    responses: [
+                        { text: ' месяцев', points: 0 },
+                    ]
+                }, {
+                    text: ["Какие из этих цветов есть в радуге?"],
+                    id: 3,
+                    responses: [
+                        { text: 'Красный', points: 4 },
+                        { text: 'Розовый', points: -3 },
+                        { text: 'Зелёный', points: 5 },
+                    ]
+                }, {
+                    text: ["Последняя обнаруженная частица Стандартной элементарной модели"],
+                    id: 4,
+                    responses: [
+                        { text: 'Мьюонное нейтрино', points: 6 },
+                        { text: 'Гравитон', points: 7 },
+                        { text: 'Бозон Хиггса', points: 8 },
+                    ]
+                }, {
+                    text: ["Ответ на главный вопрос жизни, вселенной и всего такого"],
+                    id: 5,
+                    responses: [
+                        { points: 0 },
+                    ]
+                }
+            ],
+        }
+    },
+    components: {
+        Form,
+        Field,
+        ErrorMessage,
+    },
+    methods: {
+        updateRegistration() {
+            this.renderRegistration.shift();
+            this.renderQuestions.push(1);
+            console.log(this.renderRegistration, this.renderQuestions);
+        },
+        next() {
+            this.questionIndex++;
+        },
+        prev() {
+            this.questionIndex--;
+        },
+        onSubmit(values) {
+            console.log(JSON.stringify(values, null, 2));
+        },
+        validateName(value) {
+            if (!value) {
+                return 'Это поле необходимо для заполнения';
+            }
+            const regex = /^([\w]{3,})+\s+([\w\s]{3,})+$/i;
+            if (!regex.test(value)) {
+                return 'Неккоретное имя';
+            }
+            return true;
+        },
+        validateEmail(value) {
+            if (!value) {
+                return 'Это поле необходимо для заполнения';
+            }
+            const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+            if (!regex.test(value)) {
+                return 'Неверный email адрес';
+            }
+            return true;
+        },
+        validateTel(value) {
+            if (!value) {
+                return 'Это поле необходимо для заполнения';
+            }
+            const regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+            if (!regex.test(value)) {
+                return 'Неверный номер телефона';
+            }
+            return true;
+        },
+        mounted() {
+            if (localStorage.name) this.name = localStorage.name;
+            if (localStorage.email) this.email = localStorage.email;
+            if (localStorage.tel) this.tel = localStorage.tel;
+            if (localStorage.radios) this.radios = localStorage.radios;
+            if (localStorage.results) this.results = localStorage.results;
+        },
+        addCurrentResults() {
+            this.months === 12 ? this.months = 5 : this.months = 0;
+            this.universe === '42' ? this.universe = 10 : this.universe = 0;
+            const countedRainbows = this.rainbow = this.rainbow.reduce((acc, curr) => acc + curr, 0);
+            this.results.push(this.radios, this.months, countedRainbows, this.particles, this.universe);
+            this.results = this.results.reduce((acc, curr) => acc + curr, 0) + '/35';
+            localStorage.results = JSON.stringify(this.results);
+        },
+        persist() {
+            localStorage.name = JSON.stringify(this.name);
+            localStorage.email = JSON.stringify(this.email);
+            localStorage.tel = JSON.stringify(this.tel);
+        },
+    },
+    computed: {
+        isDisabled() {
+            return (this.name.length <= 3 || this.email.length <= 6 || this.tel.length < 8) ? true : false
+        },
+    },
+}
+</script>
+
+<style scoped>
+#registration {
+    margin-top: 20px;
+    height: 90.6vh;
+    background-color: #fff;
+}
+
+.container {
+    margin-left: auto;
+    margin-right: auto;
+    width: 1400px;
+}
+
+h4 {
+    font-weight: 500;
+    color: #636B75;
+    text-align: left;
+    padding-top: 30px;
+}
+
+.errors {
+    margin-bottom: 2.5vh;
+    color: rgb(241, 56, 56);
+}
+
+.fields {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-right: -50%;
+    transform: translate(-50%, -50%);
+    width: 20%;
+    display: flex;
+    flex-direction: column;
+    margin: auto;
+}
+
+.fields input {
+    margin-bottom: 2.5vh;
+    border-radius: 5px;
+    border: 1px solid #94aae7;
+    padding: 8px 12px;
+}
+
+.buttons {
+    margin-top: 10px;
+    width: 100%;
+    text-align: center;
+    display: grid;
+    grid-template-columns: 200px auto;
+}
+
+.button-back,
+.button-login {
+    text-decoration: none;
+    text-align: center;
+    transition: .2s;
+    font-size: 16px;
+    font-weight: 500;
+    padding: 12px;
+    cursor: pointer;
+    border-radius: 10px;
+    color: rgb(255, 255, 255);
+    border: none;
+}
+
+.button-login {
+    margin-left: 50px;
+    background-color: #27AE60;
+}
+
+.button-back {
+    background-color: #2F54EB;
+}
+
+
+.button-login:disabled {
+    background-color: rgb(161, 161, 161);
+    cursor: not-allowed;
+    opacity: 0.75;
+}
+
+.button-login:disabled:hover {
+    background-color: rgb(161, 161, 161);
+    cursor: not-allowed;
+    opacity: 0.75;
+    transform: scale(1.00);
+}
+
+.button-back:hover {
+    transition: .2s;
+    transform: scale(1.03);
+    background-color: #2445ca;
+}
+
+.button-login:hover {
+    transition: .2s;
+    transform: scale(1.03);
+    background-color: #1f914e;
+}
+
+#questions {
+    width: 100%;
+    height: 90.6vh;
+    background-color: #fff;
+}
+
+.progress-step {
+    width: 145.2px;
+    background-color: #C1C9D2;
+    border: none;
+    margin-right: 10px;
+    border-radius: 10px;
+    height: 4px;
+}
+
+.progressStepCurrent {
+    width: 145.2px;
+    background-color: #5469D4;
+    border: none;
+    margin-right: 10px;
+    border-radius: 10px;
+    height: 4px;
+}
+
+.progressbar {
+    text-align: left;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+}
+
+.progress-point {
+    align-items: center;
+    display: flex;
+    cursor: pointer;
+}
+
+.points {
+    margin-right: 5px;
+}
+
+.steps h2 {
+    font-size: 14px;
+}
+
+h2 {
+    font-weight: 400;
+    color: #697386;
+}
+
+.content {
+    margin-left: auto;
+    margin-right: auto;
+    width: 1400px;
+}
+
+.container-quest {
+    width: 65%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+p {
+    font-size: 25px;
+}
+
+.quests {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.quests p {
+    max-width: 300px;
+    font-size: 20px;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 24px;
+}
+
+.choose {
+    margin-left: 20px;
+    margin-right: 40px;
+}
+
+.choose input {
+    border: 1px solid rgb(206, 206, 206);
+    border-radius: 10px;
+    padding: 12px;
+}
+
+.select {
+    margin-right: 50px;
+    font-size: 16px;
+    border: 1px solid rgb(206, 206, 206);
+    border-radius: 10px;
+    padding: 12px;
+}
+
+.btn-next,
+.btn-prev {
+    margin-right: 15px;
+    text-decoration: none;
+    font-size: 15px;
+    transition: .2s;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 15px;
+    gap: 4px;
+    cursor: pointer;
+    border-radius: 10px;
+    color: rgb(255, 255, 255);
+    border: none;
+}
+
+.btn-next {
+    background-color: #27AE60;
+}
+
+.btn-prev {
+    background-color: #2F54EB;
+}
+
+
+.btn-next:hover {
+    background-color: #228d4f;
+}
+
+.btn-prev:hover {
+    background-color: #2a47bb;
+}
+
+.btn-next:hover,
+.btn-prev:hover {
+    transition: .2s;
+    transform: scale(1.03);
+}
+</style>
